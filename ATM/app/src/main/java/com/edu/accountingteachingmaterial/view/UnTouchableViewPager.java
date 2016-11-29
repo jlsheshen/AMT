44d08500825fc6614d16baa9ea98fc52c9b6df52
WIN-DU1NKRICETH\Administrator <505280855@qq.com>
2016-11-29 10:13:41 +0800

    11.29 第一次冲突合并

--- ATM/app/src/main/java/com/edu/accountingteachingmaterial/view/UnTouchableViewPager.java
+++ ATM/app/src/main/java/com/edu/accountingteachingmaterial/view/UnTouchableViewPager.java
@@ -5,6 +5,8 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import com.edu.accountingteachingmaterial.constant.ClassContstant;
+
 /**
  * 不能触摸滑动的ViewPager
  * 
@@ -29,14 +31,17 @@ public class UnTouchableViewPager extends ViewPager {
 
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent arg0) {
-		if (type == ClassContstant||type == 9) {
+		if (type == ClassContstant.SUBJECT_BILL||type == ClassContstant.SUBJECT_GROUP_BILL||type == ClassContstant.SUBJECT_ENTRY) {
 			return false;
 		}
 		return super.onInterceptTouchEvent(arg0);
 	}
 
 	@Override
-	public boolean onTouchEvent(MotionEvent arg0) {
-		return false;
+	public boolean onTouchEvent(MotionEvent ev) {
+		if (type == ClassContstant.SUBJECT_BILL||type == ClassContstant.SUBJECT_GROUP_BILL||type == ClassContstant.SUBJECT_ENTRY) {
+			return false;
+		}
+		return super.onTouchEvent(ev);
 	}
 }
